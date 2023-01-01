764cb1aefeffb5f005aa7cbab3722d0f39edb75f
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    minor

--- src/flatmap.jl
+++ src/flatmap.jl
@@ -18,15 +18,15 @@ end
 
 
 # rougly reduce(vcat, map(a -> map(b -> f_in(a, b), f_out(a)), A))
-function flatmap(f_out::Function, f_in::Function, a)
-    TO = eltype(Base.promote_op(f_out, eltype(a)))
-    T = Base.promote_op(f_in, eltype(a), TO)
-    it = iterate(a)
+function flatmap(f_out::Function, f_in::Function, A)
+    TO = eltype(Base.promote_op(f_out, eltype(A)))
+    T = Base.promote_op(f_in, eltype(A), TO)
+    it = iterate(A)
     if isnothing(it)
-        return _empty_from_type(Base.promote_op(f_out, eltype(a)), T)
+        return _empty_from_type(Base.promote_op(f_out, eltype(A)), T)
     end
     afirst, state = it
-    arest = Iterators.rest(a, state)
+    arest = Iterators.rest(A, state)
     out = _similar_with_content(map(y -> f_in(afirst, y), f_out(afirst)), T)
     for x in arest
         for y in f_out(x)
@@ -36,14 +36,14 @@ function flatmap(f_out::Function, f_in::Function, a)
     return out
 end
 
-function flatmap(f::Function, a)
-    T = eltype(Base.promote_op(f, eltype(a)))
-    it = iterate(a)
+function flatmap(f::Function, A)
+    T = eltype(Base.promote_op(f, eltype(A)))
+    it = iterate(A)
     if isnothing(it)
-        return _empty_from_type(Base.promote_op(f, eltype(a)), T)
+        return _empty_from_type(Base.promote_op(f, eltype(A)), T)
     end
     afirst, state = it
-    arest = Iterators.rest(a, state)
+    arest = Iterators.rest(A, state)
     out = _similar_with_content(f(afirst), T)
     for x in arest
         _out = append!(out, f(x))
@@ -60,5 +60,5 @@ _empty_from_type(::Type, ::Type{T}) where {T} = T[]
 _empty_from_type(::Type{AT}, ::Type{T}) where {AT <: AbstractArray, T} = similar(AT, 0)
 
 
-flatten(x) = flatmap(identity, x)
-flatten!(out, x) = flatmap!(identity, out, x)
+flatten(A) = flatmap(identity, A)
+flatten!(out, A) = flatmap!(identity, out, A)
